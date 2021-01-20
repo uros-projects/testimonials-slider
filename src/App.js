c9eda4ef773a5188cc878bfdd113c6a4e2c67839
Uros <uros.tomasevic.work@gmail.com>
2021-01-20 22:43:07 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
+
+// data/people params: id, image, name, title, quote
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [people, setPeople] = useState(data)
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        // This effect syncs index value with personIndex
+        // value for edge cases
+        const lastIndex = people.length - 1;
+
+        if(index < 0) {
+            setIndex(lastIndex)
+        }
+        if(index > lastIndex) {
+            setIndex(0)
+        }
+    }, [index, people])
+
+    useEffect(() => {
+        // This effect handles automatic slideshow
+        let slider = setInterval(() => {
+            setIndex(index + 1);            
+        }, 2400);
+
+        return () => clearInterval(slider);
+    }, [index])
+
+    return (
+        <main>
+            <section className='section'>
+                <div className='title'>
+                    <h2><span>/</span>reviews</h2>
+                </div>
+
+                <div className='section-center'>
+                    {people.map( (person, personIndex) => {
+                        const { id, image, name, title, quote } = person;
+
+                        let position = 'nextSlide';
+
+                        if(personIndex === index) {
+                            position = 'activeSlide';
+                        }
+                            
+                        if(personIndex === index - 1 || 
+                            (index === 0 && personIndex === people.length - 1)) {
+                                position = 'lastSlide';
+                            }                        
+
+                        return (
+                            <article key={id} className={position}>
+                                <img src={image} alt={name} className='person-img' />
+                                <h4>{name}</h4>
+                                <p className='title'>{title}</p>
+                                <p className='text'>{quote}</p>
+                                <FaQuoteRight className='icon' />
+                            </article>
+                        )
+                    })}
+                    <button className='prev' onClick={() => setIndex(prevIndex => prevIndex - 1)}>
+                        <FiChevronLeft />
+                    </button>
+                    <button className='next' onClick={() => setIndex(prevIndex => prevIndex + 1)}>
+                        <FiChevronRight />
+                    </button>
+                </div>
+            </section>           
+        </main>
+    );
 }
 
 export default App;
